02635e97645fee2f746dda35f45fd5b7762cb11f
Mdmovies <91911991+Mdmovies@users.noreply.github.com>
2021-10-05 00:24:58 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('join our channel', url=f'https://t.me/MDMOVISES')
+        InlineKeyboardButton('🔰join our channel🔰', url=f'https://t.me/MDMOVISES')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
