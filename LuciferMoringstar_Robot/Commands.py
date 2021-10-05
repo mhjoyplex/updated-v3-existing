6282f6b1d3bbc53dd4704dcf8b8933e8b661844c
Mdmovies <91911991+Mdmovies@users.noreply.github.com>
2021-10-05 18:08:11 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -85,7 +85,7 @@ async def start(bot, message):
                 buttons = [
                     [
                         InlineKeyboardButton('🎖 join our channel 🎖', url='https://t.me/MDMOVISES')
-                    ],[ InlineKeyboardButton('🔰share our group🔰', url=f'https://t.me/share/url?url=HTTPS://t.me/malayalamdubbedmoviegroup')     
+                    ],[ InlineKeyboardButton('🔰share our group🔰', url=f'https://t.me/share/url?url=HTTPS://t.me/malayalamdubbedmoviegroup&text=ALL-MALAYALAM-MOVIES-GROUP')     
                     ]
                     ]
                 await bot.send_cached_media(
