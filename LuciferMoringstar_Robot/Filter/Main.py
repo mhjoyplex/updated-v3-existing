82ad67f0594344eca03db931dcd38eaa059f4df1
Mdmovies <91911991+Mdmovies@users.noreply.github.com>
2021-10-10 23:59:54 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -145,7 +145,7 @@ async def group(client, message):
             buttons = btn
             buttons.append(
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")],
-                [InlineKeyboardButton('join our channel',url="HTTPS://t.me/mdmovises)]
+                [InlineKeyboardButton('join our channel',url='HTTPS://t.me/mdmovises')]
             )
             poster=None
             if API_KEY:
